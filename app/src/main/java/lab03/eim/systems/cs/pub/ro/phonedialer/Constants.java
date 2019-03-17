84ba9c8cab2cad2c6aba0200505fd5f559723cd6
Cosmin Stanciu <stanciu.cossmin@gmail.com>
2019-03-18 01:13:36 +0200

    Tasks 1-5

---
+++ app/src/main/java/lab03/eim/systems/cs/pub/ro/phonedialer/Constants.java
@@ -0,0 +1,20 @@
+package lab03.eim.systems.cs.pub.ro.phonedialer;
+
+public interface Constants {
+
+    final public static int buttonIds[] = {
+            R.id.number_0_button,
+            R.id.number_1_button,
+            R.id.number_2_button,
+            R.id.number_3_button,
+            R.id.number_4_button,
+            R.id.number_5_button,
+            R.id.number_6_button,
+            R.id.number_7_button,
+            R.id.number_8_button,
+            R.id.number_9_button,
+            R.id.star_button,
+            R.id.diez_button
+    };
+    final public static int PERMISSION_REQUEST_CALL_PHONE = 1;
+}
